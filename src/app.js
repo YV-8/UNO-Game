c9b758b6b9fdcfbda2efa2bb79294c970386cfcf
Moni <monica.escobar0125@jala.university>
2026-07-22 19:36:44 -0400

    add helper and moificated app.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import playerRoute from './presentation/routes/player.route.js'
 import gameRoute from './presentation/routes/game.router.js'
 import cardsRoute from './presentation/routes/cards.router.js'
 import scoreRoute from './presentation/routes/score.router.js'
+import authRoute from './presentation/routes/auth.route.js'
 import {errorHandler} from './middlewares/errorHandler.middleware.js'
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/players', playerRoute);
-
+app.use('/api/auth', authRoute);
 app.use('/api/games', gameRoute);
 app.use('/api/cards', cardsRoute);
 app.use('/api/scores', scoreRoute);
--- src/helpers/responseHandler.middleware.js
+++ src/helpers/responseHandler.middleware.js
@@ -8,7 +8,6 @@
  */
 export const sendSuccess = (res, statusCode = 200, message = 'Success', data = null) => {
   return res.status(statusCode).json({
-    success: true,
     message,
     ...(data && { data })
   });
